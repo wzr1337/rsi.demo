65898edf267bd244f9e076385fe0837af2852e8e
Dr. Patrick Bartsch <bartsch98@gmail.com>
2018-02-01 16:19:23 +0100

    initial

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { PluginLoader, RsiServer, ServiceRegistry } from '@rsi/server';
+import { join } from 'path';
+
+const DEFAULTRUNOPTIONS = {
+    port: 3000,
+    verbosity: 'silly',
+    base: '',
+    serviceRegistry: 'http://localhost:3600'
+};
+
+/**
+ * parse command line options
+ */
+const commandLineArgs = require('command-line-args')
+const optionDefinitions = [
+    { name: 'verbosity', alias: 'v', type: String },
+    { name: 'port', alias: 'p', type: Number },
+    { name: 'base', alias: 'b', type: String },
+    { name: 'include-addons', alias: 'a', type: String },
+    { name: 'exclude-plugins', alias: 'e', type: String },
+    { name: 'serviceRegistry', alias: 's', type: String }
+]
+const cla = commandLineArgs(optionDefinitions);
+const options = Object.assign(DEFAULTRUNOPTIONS, cla);
+const url = require('url');
+const u = url.parse(options.serviceRegistry);
+const serviceRegistryPort: number = u.port;
+
+/** end parse command line argunments */
+
+
+if (options.serviceRegistry || options.serviceRegistry !== '') {
+    const serviceRegistry: ServiceRegistry = new ServiceRegistry(serviceRegistryPort);
+    serviceRegistry.init();
+}
+
+const server: RsiServer = new RsiServer();
+server.run(Object.assign(DEFAULTRUNOPTIONS, cla));
+
+if (!cla.hasOwnProperty('exclude-plugins')) {
+    const plugins: PluginLoader = new PluginLoader(server);
+    plugins.loadPlugins(join(__dirname, '..', 'node_modules', '@rsi-plugins', 'cq.all', 'dist'));
+}
+
