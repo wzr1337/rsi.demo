a600f53b536bcb9f71bf1954d3b4084ec28d651b
Dr. Patrick Bartsch <bartsch98@gmail.com>
2018-02-04 12:45:27 +0100

    fix(): remove service registry

---
+++ README.md
@@ -0,0 +1,35 @@
+# RSI Demo application
+
+This project implements the Volkswagen Infotainment Web Interface (viwi/RSI) as published under [https://www.w3.org/Submission/2016/01/](https://www.w3.org/Submission/2016/01/). It uses the base modules
+
+* [@RSI/server](https://github.com/wzr1337/rsi.server)
+* [@RSI/medialibrary](https://github.com/wzr1337/rsi-plugins.medialibrary)
+
+
+## Build the software
+As this project uses TypeScript, you need to complie it into regular JavaScript first using 
+
+```sh
+$ npm run build
+```
+
+## Run it
+
+To run the server, use
+
+```sh
+$ http_proxy='' npm run start
+```
+
+**Note** Please ensure that proxy settings are set appropriately for your environment.
+
+after you `npm run build` it
+
+
+### Available command line arguments
+
+| long parameter | short parameter | type   | description                                    |
+|----------------|-----------------|--------|------------------------------------------------|
+| --port         | -p              | number | the port number to listen on                   |
+| --base         | -b              | string | the base url                                   |
+| --verbosity    | -v              | string | the winston log level to plot into the console |
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { PluginLoader, RsiServer, ServiceRegistry } from '@rsi/server';
+import * as ml from '@rsi-plugins/medialibrary';
 import { join } from 'path';
 
 const DEFAULTRUNOPTIONS = {
   port: 3000,
   verbosity: 'silly',
-  base: '',
-  serviceRegistry: 'http://localhost:3600'
+  base: ''
 };
 
 /**
@@ -16,32 +16,17 @@ const optionDefinitions = [
   { name: 'verbosity', alias: 'v', type: String },
   { name: 'port', alias: 'p', type: Number },
   { name: 'base', alias: 'b', type: String },
-  { name: 'include-addons', alias: 'a', type: String },
-  { name: 'exclude-plugins', alias: 'e', type: String },
   { name: 'serviceRegistry', alias: 's', type: String }
 ]
 const cla = commandLineArgs(optionDefinitions);
-const options = Object.assign(DEFAULTRUNOPTIONS, cla);
-const url = require('url');
-const u = url.parse(options.serviceRegistry);
-const serviceRegistryPort: number = u.port;
-
 /** end parse command line argunments */
 
-
-if (options.serviceRegistry || options.serviceRegistry !== '') {
-  const serviceRegistry: ServiceRegistry = new ServiceRegistry(serviceRegistryPort);
-  serviceRegistry.init();
-}
-
 const server: RsiServer = new RsiServer();
-
 server.run(Object.assign(DEFAULTRUNOPTIONS, cla));
 
 /**
  * load plugins and add them to the server
  */
-import * as ml from '@rsi-plugins/medialibrary';
 const medialibraryPlugins = ml.getPlugins();
 
 for (let index = 0; index < medialibraryPlugins.length; index++) {
